c5e556dbc4bc13cb835ad682a7dc698a33f525d2
root <aravind.arali@gmail.com>
2022-06-07 15:29:56 +0530

    check commit

--- multi-controller-switch/algorithm.py
+++ multi-controller-switch/algorithm.py
@@ -1,11 +1,11 @@
 import random
 
 
-c1={'id': 1, 'name': 'c1', 'port': '6633', 'switches': {1: {'name': 's1', 'packet_in_count': 10}, 2: {'name': 's2', 'packet_in_count': 0}, 3: {'name': 's3', 'packet_in_count': 0}, 4: {'name': 's4', 'packet_in_count': 0}, 5: {'name': 's5', 'packet_in_count': 0}, 6: {'name': 's6', 'packet_in_count': 0}}}
+# c1={'id': 1, 'name': 'c1', 'port': '6633', 'switches': {1: {'name': 's1', 'packet_in_count': 10}, 2: {'name': 's2', 'packet_in_count': 0}, 3: {'name': 's3', 'packet_in_count': 0}, 4: {'name': 's4', 'packet_in_count': 0}, 5: {'name': 's5', 'packet_in_count': 0}, 6: {'name': 's6', 'packet_in_count': 0}}}
 
-c2={'id': 2, 'name': 'c2', 'port': '6634', 'switches': {1: {'name': 's1', 'packet_in_count': 10}, 2: {'name': 's2', 'packet_in_count': 2}, 3: {'name': 's3', 'packet_in_count': 0}, 4: {'name': 's4', 'packet_in_count': 0}, 5: {'name': 's5', 'packet_in_count': 0}, 6: {'name': 's6', 'packet_in_count': 0}}}
+# c2={'id': 2, 'name': 'c2', 'port': '6634', 'switches': {1: {'name': 's1', 'packet_in_count': 10}, 2: {'name': 's2', 'packet_in_count': 2}, 3: {'name': 's3', 'packet_in_count': 0}, 4: {'name': 's4', 'packet_in_count': 0}, 5: {'name': 's5', 'packet_in_count': 0}, 6: {'name': 's6', 'packet_in_count': 0}}}
 
-c3={'id': 3, 'name': 'c3', 'port': '6635', 'switches': {1: {'name': 's1', 'packet_in_count': 10}, 2: {'name': 's2', 'packet_in_count': 15}, 3: {'name': 's3', 'packet_in_count': 0}, 4: {'name': 's4', 'packet_in_count': 10}, 5: {'name': 's5', 'packet_in_count': 0}, 6: {'name': 's6', 'packet_in_count': 0}}}
+# c3={'id': 3, 'name': 'c3', 'port': '6635', 'switches': {1: {'name': 's1', 'packet_in_count': 10}, 2: {'name': 's2', 'packet_in_count': 15}, 3: {'name': 's3', 'packet_in_count': 0}, 4: {'name': 's4', 'packet_in_count': 10}, 5: {'name': 's5', 'packet_in_count': 0}, 6: {'name': 's6', 'packet_in_count': 0}}}
 
 def total_packet_in_count(controller):
     sum=0
